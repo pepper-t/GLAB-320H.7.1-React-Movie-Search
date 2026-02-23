146dd9a0d65cc1a0f9e6742d3f36401424817fa3
Aisha Shaheed <ps.ashaheed@outlook.com>
2026-02-23 17:43:48 -0500

    updated file with try catch error

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,28 @@ export default function App() {
 
   // Function to get movies
   const getMovie = async(searchTerm) => {
+    try {
     // Make fetch request and store the response
     const response = await fetch(
       `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
     );
+
     // Parse JSON response into a JavaScript object
     const data = await response.json();
     // Set the Movie state to the received data
     setMovie(data);
+  }catch(e) {
+    console.error(e)
+  }
   };
+
+
+ // This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
+
 
   // We pass the getMovie function as a prop called moviesearch
   // We pass movie as props to movie display
