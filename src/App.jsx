3b65cbeb11fb97077b202f4d9bd63b8be975bab1
Aisha Shaheed <ps.ashaheed@outlook.com>
2026-02-23 17:27:59 -0500

    uploaded files from src folder to git

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import vite from "../public/vite.svg";
+import "./App.css";
+
+// Import our components
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  // Constant with your API Key
+  const apiKey = "ff16042";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async(searchTerm) => {
+    // Make fetch request and store the response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a JavaScript object
+    const data = await response.json();
+    // Set the Movie state to the received data
+    setMovie(data);
+  };
+
+  // We pass the getMovie function as a prop called moviesearch
+  // We pass movie as props to movie display
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
+}
